56b6dcd4b56f3b564b0aa648285d91c3ce6a80c9
Sushant Kondguli <thisissush@gmail.com>
2020-03-08 13:26:50 -0700

    Update main.cpp

--- OpenGLHelloWorld/main.cpp
+++ OpenGLHelloWorld/main.cpp
@@ -66,9 +66,9 @@ int main(int argc, const char * argv[]) {
     CreateTriangle();
     // build and compile our shader program
     // ------------------------------------
-    Shader tShader("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/triangle.vs", "/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/triangle.fs"); // you can name your shader files however you like
-    unsigned int texture0 = LoadTexture("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/container.jpg", GL_RGB, GL_CLAMP_TO_EDGE, GL_LINEAR);
-    unsigned int texture1 = LoadTexture("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/awesomeface.png", GL_RGBA, GL_REPEAT, GL_LINEAR);
+    Shader tShader("triangle.vs", "triangle.fs"); // you can name your shader files however you like
+    unsigned int texture0 = LoadTexture("container.jpg", GL_RGB, GL_CLAMP_TO_EDGE, GL_LINEAR);
+    unsigned int texture1 = LoadTexture("awesomeface.png", GL_RGBA, GL_REPEAT, GL_LINEAR);
     
     // CompileShaders();
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -86,7 +86,8 @@ int main(int argc, const char * argv[]) {
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         float redValue = (sin(timeValue) / 2.0f) + 0.5f;
-        float delta_x  = (sin(timeValue) / 2.0f) + 0.5f;
+        float delta_x  = (sin(timeValue) / 2.0f);
+        float tex_ratio= (sin(timeValue) / 2.0f) + 0.5f;
         
         // render the OpenGL, put all your OpenGL code here
         // glUseProgram(shader[0]);
@@ -96,31 +97,15 @@ int main(int argc, const char * argv[]) {
         glActiveTexture(GL_TEXTURE1);
             glBindTexture(GL_TEXTURE_2D, texture1);
         tShader.use();
-        tShader.setFloat4("userColor", 1.0f, greenValue, 1.0f, 1.0);
-        tShader.setInt("texture1", 0);
-        tShader.setInt("texture2", 1);
-            // int colorLoc0 = glGetUniformLocation(shader[0], "userColor");
-            // int delta_x0  = glGetUniformLocation(shader[0], "x_loc");
-            // glUniform4f(colorLoc0, 0.0f, greenValue, 0.0f, 1.0f);
-            // glUniform1f(delta_x0, delta_x);
-            glBindVertexArray(VAO[0]);
-                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-                // glDrawArrays(GL_TRIANGLES, 3, 3);
-            // glBindVertexArray(0);
-        // glUseProgram(0);
-        // glUseProgram(shader[1]);
-        // tShader.use();
-        tShader.setFloat4("userColor", redValue, 1.0f, 1.0f, 1.0);
-        tShader.setFloat("x_loc", -0.5 + delta_x);
-        tShader.setFloat("textureRatio", delta_x);
-            // int colorLoc1 = glGetUniformLocation(shader[1], "userColor");
-            // int delta_x1  = glGetUniformLocation(shader[1], "x_loc");
-            // glUniform4f(colorLoc1, redValue, 0.0f, 0.0f, 1.0f);
-            // glUniform1f(delta_x1, -delta_x);
-            // glBindVertexArray(VAO[1]);
-                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-                // glDrawArrays(GL_TRIANGLES, 15, 3);
-            // glBindVertexArray(0);
+            // tShader.setFloat4("userColor", 1.0f, greenValue, 1.0f, 1.0);
+            tShader.setFloat4("userColor", redValue, 1.0f, 1.0f, 1.0);
+            tShader.setInt("texture1", 0);
+            tShader.setInt("texture2", 1);
+            tShader.setFloat("x_loc", delta_x);
+            tShader.setFloat("textureRatio", tex_ratio);
+        
+        glBindVertexArray(VAO[0]);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glUseProgram(0);
         
         // swap front and back buffers
