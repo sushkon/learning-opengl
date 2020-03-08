9139eb85bdc05b1f2b52cb88228915e5d70748af
Sushant Kondguli <thisissush@gmail.com>
2020-03-08 14:04:57 -0700

    Update main.cpp

--- helloTriangle/main.cpp
+++ helloTriangle/main.cpp
@@ -63,14 +63,10 @@ int main(int argc, const char * argv[]) {
     // setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
     
+    // generate geometry
     CreateTriangle();
-    // build and compile our shader program
-    // ------------------------------------
-    Shader tShader("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/triangle.vs", "/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/triangle.fs"); // you can name your shader files however you like
-    unsigned int texture0 = LoadTexture("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/container.jpg", GL_RGB, GL_CLAMP_TO_EDGE, GL_LINEAR);
-    unsigned int texture1 = LoadTexture("/Users/sushant/Documents/OpenGLHelloWorld/OpenGLHelloWorld/awesomeface.png", GL_RGBA, GL_REPEAT, GL_LINEAR);
-    
-    // CompileShaders();
+    // link shaders
+    CompileShaders();
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     // loop until window closed
@@ -88,39 +84,25 @@ int main(int argc, const char * argv[]) {
         float redValue = (sin(timeValue) / 2.0f) + 0.5f;
         float delta_x  = (sin(timeValue) / 2.0f) + 0.5f;
         
-        // render the OpenGL, put all your OpenGL code here
-        // glUseProgram(shader[0]);
-        // bind and activate textures
-        glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, texture0);
-        glActiveTexture(GL_TEXTURE1);
-            glBindTexture(GL_TEXTURE_2D, texture1);
-        tShader.use();
-        tShader.setFloat4("userColor", 1.0f, greenValue, 1.0f, 1.0);
-        tShader.setInt("texture1", 0);
-        tShader.setInt("texture2", 1);
-            // int colorLoc0 = glGetUniformLocation(shader[0], "userColor");
-            // int delta_x0  = glGetUniformLocation(shader[0], "x_loc");
-            // glUniform4f(colorLoc0, 0.0f, greenValue, 0.0f, 1.0f);
-            // glUniform1f(delta_x0, delta_x);
+        glUseProgram(shader[0]);
+            int colorLoc0 = glGetUniformLocation(shader[0], "userColor");
+            int delta_x0  = glGetUniformLocation(shader[0], "x_loc");
+            glUniform4f(colorLoc0, 0.0f, greenValue, 0.0f, 1.0f);
+            glUniform1f(delta_x0, delta_x);
             glBindVertexArray(VAO[0]);
                 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
                 // glDrawArrays(GL_TRIANGLES, 3, 3);
-            // glBindVertexArray(0);
-        // glUseProgram(0);
-        // glUseProgram(shader[1]);
-        // tShader.use();
-        tShader.setFloat4("userColor", redValue, 1.0f, 1.0f, 1.0);
-        tShader.setFloat("x_loc", -0.5 + delta_x);
-        tShader.setFloat("textureRatio", delta_x);
-            // int colorLoc1 = glGetUniformLocation(shader[1], "userColor");
-            // int delta_x1  = glGetUniformLocation(shader[1], "x_loc");
-            // glUniform4f(colorLoc1, redValue, 0.0f, 0.0f, 1.0f);
-            // glUniform1f(delta_x1, -delta_x);
-            // glBindVertexArray(VAO[1]);
+            glBindVertexArray(0);
+        glUseProgram(0);
+        glUseProgram(shader[1]);
+            int colorLoc1 = glGetUniformLocation(shader[1], "userColor");
+            int delta_x1  = glGetUniformLocation(shader[1], "x_loc");
+            glUniform4f(colorLoc1, redValue, 0.0f, 0.0f, 1.0f);
+            glUniform1f(delta_x1, -delta_x);
+            glBindVertexArray(VAO[1]);
                 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
                 // glDrawArrays(GL_TRIANGLES, 15, 3);
-            // glBindVertexArray(0);
+            glBindVertexArray(0);
         glUseProgram(0);
         
         // swap front and back buffers
